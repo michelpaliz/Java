4f2b93ec9110a21f72325190b3966856540958e3
Michael <michelpaliz@hotmail.com>
2022-01-29 15:44:30 +0100

    Apply more changes that i've created

--- tema7Correccion/tema07estructurasdatos-master/Ejercicio04.java
+++ tema7Correccion/tema07estructurasdatos-master/Ejercicio04.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
-public class Ejercicio04 {
-   public static void execute() {
-      String frase;
-      String[] palabras;
-      int[] contador;
-      System.out.print("Dime la frase: ");
-      frase = Main.lector.nextLine();
-      palabras = frase.split(" ");
-      for(int i = 0; i < palabras.length; i++) {
-         contador = contarVocalesYConsonantes(palabras[i]);
-         System.out.println("Palabra: " + palabras[i]);
-         System.out.println("Vocales: " + contador[0]);
-         System.out.println("Consonantes: " + contador[1]);
-      }
-   }
 
-   /**
+public class Ejercicio04 {
+    public static void execute() {
+        String frase;
+        String[] palabras;
+        int[] contador;
+        System.out.print("Dime la frase: ");
+        frase = Main.lector.nextLine();
+        palabras = frase.split(" ");
+        for (int i = 0; i < palabras.length; i++) {
+            contador = contarVocalesYConsonantes(palabras[i]);
+            System.out.println("Palabra: " + palabras[i]);
+            System.out.println("Vocales: " + contador[0]);
+            System.out.println("Consonantes: " + contador[1]);
+        }
+    }
+
+    /**
      * Devuelve el número de vocales y el número de consonantes de una frase
      * 
      * @param frase
@@ -29,9 +30,9 @@ public class Ejercicio04 {
         int[] resultado = { 0, 0 };
         frase = frase.toLowerCase();
         for (int i = 0; i < frase.length(); i++) {
-            if(vocales.indexOf(frase.charAt(i)) >= 0)
+            if (vocales.indexOf(frase.charAt(i)) >= 0)
                 nVocales++;
-            else if(Character.isLetter(frase.charAt(i)))
+            else if (Character.isLetter(frase.charAt(i)))
                 nConsonantes++;
         }
         resultado[0] = nVocales;
--- tema7Correccion/tema07estructurasdatos-master/Main.java
+++ tema7Correccion/tema07estructurasdatos-master/Main.java
@@ -1,36 +1,60 @@
 import java.util.Scanner;
+
 public class Main {
    public static Scanner lector;
+
    public static void main(String[] args) {
       lector = new Scanner(System.in);
-      Ejercicio01.execute();
-      //Ejercicio02.execute();
-      //Ejercicio03.execute();
-      //Ejercicio04.execute();
-      //Ejercicio05.execute();
-      //Ejercicio06.execute();
-      //Ejercicio07.execute();
-      //Ejercicio08.execute();
-      //Ejercicio09.execute();
-      //Ejercicio10.execute();
-      //Ejercicio11.execute();
-      //Ejercicio12.execute();
-      //Ejercicio13.execute();
-      //Ejercicio14.execute();
-      //Ejercicio15.execute();
-      //Ejercicio16.execute();
-      //Ejercicio17.execute();
-      //Ejercicio18.execute();
-      //Ejercicio19.execute();
-      //Ejercicio20.execute();
-      //Ejercicio21.execute();
-      //Ejercicio22.execute();
-      //Ejercicio23.execute();
-      //Ejercicio24.execute();
-      //Ejercicio25.execute();
-      //Ejercicio26.execute();
-      //Ejercicio27.execute();
-      //Ejercicio28.execute();
-      //Ejercicio29.execute();
+      // *Escribe un metodo que dada una frase devuelva la misma pero con la primer
+      // *letra en mayuscula
+      // Ejercicio01.execute();
+      // *Escribe un programa que muestre el numero de vocales y el numero de
+      // *consonantes de una frase;
+      // Ejercicio02.execute();
+      // *Escribe un metodo que cuente el numero de palabras que contiene. Asumimos
+      // * que cada palabra esta separada por un solo espacio en blanco.
+      // Ejercicio03.execute();
+      // *Escribe un programa que muestre el numero de vocales y el numero de
+      // * consonantes de cada palabra de una frase;
+      // Ejercicio04.execute();
+      // *Escribe un programa que lea una frase y encuentre la palabra de mayor
+      // *longitud. El programa debe de imprimir la palabra y el numero de caracteres
+      // *que contiene;
+      // Ejercicio05.execute();
+      // *Escribe un programa que lea un frase y un factor de multiplicacion y
+      // *devuelve la frase repedita tantas veces como indique el factor de
+      // * multiplicacion.
+      // Ejercicio06.execute();
+      // *Escribe un programa que lea una frase y muestre cada palabra en una linea
+      // *diferente
+      // Ejercicio07.execute();
+      // *Escribe un programa que dada una frase escriba en 2 columnas cada palabra
+      // *seguida del numero de caracteres de continene
+      // Ejercicio08.execute();
+      // *Escribe un metodo que dada una frase devuelva otra pero con solo los
+      // * caracteres de las posiciones impares
+      // Ejercicio09.execute();
+      // *Escribe un metodo que devuelva true si una palabra es palindroma(se escribe
+      // *igual de izquierda a derecha y de derecha a izquierda y false si no lo es)
+      // Ejercicio10.execute();
+      // Ejercicio11.execute();
+      // Ejercicio12.execute();
+      // Ejercicio13.execute();
+      // Ejercicio14.execute();
+      // Ejercicio15.execute();
+      // Ejercicio16.execute();
+      // Ejercicio17.execute();
+      // Ejercicio18.execute();
+      // Ejercicio19.execute();
+      // Ejercicio20.execute();
+      // Ejercicio21.execute();
+      // Ejercicio22.execute();
+      // Ejercicio23.execute();
+      // Ejercicio24.execute();
+      // Ejercicio25.execute();
+      // Ejercicio26.execute();
+      // Ejercicio27.execute();
+      // Ejercicio28.execute();
+      Ejercicio29.execute();
    }
 }
