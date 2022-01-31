421a93974575b99e1c0cce2c4c332af66a8a860f
Michael <michelpaliz@hotmail.com>
2022-01-31 19:21:06 +0100

    changes made for subject number 8

---
+++ tema8/tema8/persona/Persona.java
@@ -0,0 +1,18 @@
+
+public class Persona {
+
+    // Atributos
+    String nombre;
+    int vida;
+    int edad;
+    int fechaNacimiento;
+    int dni;
+
+    // Metodo
+    public Persona(String nombre, int edad, int dni) {
+        this.nombre = nombre;
+        this.edad = edad;
+
+    }
+
+}
